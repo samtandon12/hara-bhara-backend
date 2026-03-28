7d14c66a1671c6d649e6ca37201c95e30e50ec92
Sanket <samtandon71@gmail.com>
2026-03-28 11:18:53 +0530

    fix mongo uri

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/hara-bhara')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
@@ -161,7 +163,6 @@ app.get('/{*path}', (req, res) => {
 });
 
 // Start server
-const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
